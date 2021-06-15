47119d0e9de16eb7123018d7aeed9acee9b68bc7
ricfreitas13 <85580507+ricfreitas13@users.noreply.github.com>
2021-06-15 13:33:26 -0300

    Add files via upload

---
+++ programação imperativa - turma 1/aula2-ex.js
@@ -0,0 +1,18 @@
+//comentário de uma linha
+
+/*comentario 
+em
+bloco
+blablablabla*/
+
+
+//declarar
+var nome;
+let outroNome;
+
+
+//atribuir
+nome='adevaldo'
+nome='henique'
+console.log(nome)
+c
